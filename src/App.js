7248a399df00bfb4a8adeef806768604f506b59f
Justus <justus.onyancha@moringaschool.com>
2023-01-16 13:11:35 +0300

    create an incremental app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 export default function App() {
   const [count, setCount] = useState(0);
   const [counter, setCounter] = useState(100);
+  const [value, setValue] = useState('');
   return (
     <>
       <div>
@@ -25,6 +26,16 @@ export default function App() {
           Decrement Button
         </button>
         <h1>counter: {counter}</h1>
+
+        {/* add string */}
+
+        <input
+          type="text"
+          value={value}
+          onChange = {(event) => {
+            setValue(event.target.value);
+          }}
+        />
       </div>
     </>
   );
